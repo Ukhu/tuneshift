99bb7444e7316ceb96a3c2a8c70205ce78dac820
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2020-09-22 16:32:40 +0100

    create new API service to handle user authorization and other API requets

---
+++ src/auth/APIService.ts
@@ -0,0 +1,138 @@
+import axios, { AxiosInstance } from 'axios';
+import querystring from 'querystring';
+import cryptoRandomString from 'crypto-random-string';
+import { SPOTIFY_CLIENT_ID, DEEZER_CLIENT_ID, CORS_PROXY_URL, Track } from '../utils/constants';
+
+class AuthService {
+  public spotifyBaseUrl: string = `${CORS_PROXY_URL}/https://api.spotify.com/v1`;
+  public spotifyAccessToken: string = '';
+  public deezerBaseUrl: string = `${CORS_PROXY_URL}/https://api.deezer.com`;
+  public deezerAccessToken: string = '';
+  public antiCSRFState: string;
+  private readonly _client: AxiosInstance;
+
+  constructor() {
+    this._client = axios.create({
+
+    });
+    this.antiCSRFState = this.generateAntiCSRFSafe();
+  }
+
+  // Authorization Methods
+  authorizeWithSpotify(cb: Function): void {
+    window.open(`https://accounts.spotify.com/authorize?${querystring.stringify({
+      client_id: SPOTIFY_CLIENT_ID,
+      response_type: 'token',
+      redirect_uri: 'http://localhost:3000/callback',
+      scope: 'playlist-modify-public',
+      state: this.antiCSRFState
+    })}`);
+
+    const handleFocus = () => {
+      window.removeEventListener('focus', handleFocus);
+      const recievedToken = localStorage.getItem('spotify_user_access_token') ?? '';
+      const recievedState = localStorage.getItem('received_anti_csrf_state') ?? '';
+
+      const decodedState = decodeURIComponent(recievedState);
+
+      if (decodedState !== this.antiCSRFState || recievedToken === '') {
+        cb('Authorization request invalid. Kindly re-athorize');
+      } else {
+        this.spotifyAccessToken = recievedToken;
+        cb(null)
+      }
+    }
+    window.addEventListener('focus', handleFocus);
+  }
+
+  authorizeWithDeezer(cb: Function): void {
+    window.open(`https://connect.deezer.com/oauth/auth.php?${querystring.stringify({
+      app_id: DEEZER_CLIENT_ID,
+      response_type: 'token',
+      redirect_uri: 'http://localhost:3000/callback',
+      perms: 'manage_library',
+      state: this.antiCSRFState
+    })}`);
+
+    const handleFocus = () => {
+      window.removeEventListener('focus', handleFocus);
+      const recievedToken = localStorage.getItem('deezer_user_access_token') ?? '';
+      const recievedState = localStorage.getItem('received_anti_csrf_state') ?? '';
+
+      const decodedState = decodeURIComponent(recievedState);
+
+      if (decodedState !== this.antiCSRFState || recievedToken === '') {
+        cb('Authorization request invalid. Kindly re-athorize');
+      } else {
+        this.spotifyAccessToken = recievedToken;
+        cb(null)
+      }
+    }
+    window.addEventListener('focus', handleFocus);
+  }
+
+  generateAntiCSRFSafe(): string {
+    const antiCsrfState = cryptoRandomString({
+      length: 20,
+      type: 'base64'
+    });
+    return antiCsrfState;
+  }
+
+  // Regular Methods
+  fetchSpotifyPlaylist(id: string): Promise<any> {
+    return this._client.get(`${this.spotifyBaseUrl}/playlists/${id}`, {
+      headers: {
+        'Authorization': `Bearer ${this.spotifyAccessToken}`
+      }
+    }).then((response) => {
+      const { name, owner, tracks, images } = response.data;
+      const playlistImage = images.filter((img: any) => img.height === 300)[0].url || images[0].url;
+      const prunedTracks: Track[] = tracks.items.filter((item: any) => item.track.type === 'track').map((item: any) => ({
+        artist: item.track.artists[0].name,
+        title: item.name
+      }))
+
+      return ({
+        owner: owner.display_name,
+        title: name,
+        image: playlistImage,
+        tracks: prunedTracks,
+        providerName: 'spotify'
+      })
+    }).catch((e) => {
+      return e.response.data.error.message || 'An error occured'
+    })
+  }
+
+  fetchDeezerPlaylist(id: string): Promise<any> {
+    return this._client.get(`${this.deezerBaseUrl}/playlist/${id}`, {
+      headers: {
+        'Authorization': `Bearer ${this.deezerAccessToken}`
+      }
+    }).then((response) => {
+      const { title, creator, tracks, picture_medium, error } = response.data;
+
+			if (error) {
+				throw new Error(error.message)
+			}
+
+			const prunedTracks: Track[] = tracks.data.filter((track: any) => track.type === 'track').map((track: any) => ({
+        artist: track.artist.name,
+        title: track.title
+      }))
+        
+      return ({
+        owner: creator.name,
+				title,
+				image: picture_medium,
+				tracks: prunedTracks,
+				providerName: 'deezer'
+      })
+    }).catch((e) => {
+      return e.response.data.error.message || 'An error occured'
+    })
+  }
+}
+
+export default AuthService;
