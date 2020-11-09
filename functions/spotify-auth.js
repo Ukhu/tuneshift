017fc88ac5a39ed9fdf5a0016b8865b66085d6af
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2020-11-09 19:18:20 +0100

    fix lingering functions deployment issue

--- functions/spotify-auth.js
+++ functions/spotify-auth.js
@@ -5,10 +5,25 @@ const SPOTIFY_URL = process.env.SPOTIFY_URL;
 const SPOTIFY_CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 const SPOTIFY_CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 
-exports.handler = async function(event, context) {
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "OPTIONS, GET",
+  "Access-Control-Max-Age": "2592000",
+  "Content-Type": "application/json"
+}
+
+exports.handler = function(event, context, callback) {
+  if (event.httpMethod === 'OPTIONS') {
+    callback(null, {
+      statusCode: '204',
+      headers
+    });
+    return;
+  }
+
   const BASE64_ENCODED_KEY = Buffer.from(SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET).toString('base64');
 
-  return axios.post(SPOTIFY_URL,
+  axios.post(SPOTIFY_URL,
     querystring.stringify({
       grant_type: 'client_credentials'
     }),{
@@ -16,16 +31,22 @@ exports.handler = async function(event, context) {
       'Content-Type': 'application/x-www-form-urlencoded',
       Authorization: 'Basic ' + BASE64_ENCODED_KEY
     }
-  }).then(axiosRes => ({
-    statusCode: 200,
-    body: {
-      access_token: axiosRes.data.access_token,
-      expires_in: axiosRes.data.expires_in
-    }
-  })).catch(axiosErr => ({
-    statusCode: axiosErr.response.status,
-    body: {
-      message: axiosErr.response.data.error_description
-    }
-  }))
+  }).then(axiosRes => {
+    callback(null, {
+      statusCode: 200,
+      body: {
+        access_token: axiosRes.data.access_token,
+        expires_in: axiosRes.data.expires_in
+      },
+      headers
+    })
+  }).catch((axiosErr) => {
+    callback(null, {
+      statusCode: axiosErr.response.status,
+      body: {
+        message: axiosErr.response.data.error_description
+      },
+      headers
+    })
+  })
 }
